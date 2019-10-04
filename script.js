e09760ca521ad0342e322b2e7c3f3ca7e5b9999f
Rhaidzsal Ali <rhaidzsal.ali@wmsu.edu.ph>
2019-10-04 10:29:37 +0800

    :sparkles: Initial loading screen has been added.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// LOADING SCREEN SCRIPT
+// We will use a fake timer that will add the fadeout class after the timer ends.
 
+const initLoadingScreen = () => {
+  setTimeout(() => {
+    document.getElementById("loading_screen").classList.add("fadeout");
+  }, 600);
+};
+
+initLoadingScreen();
