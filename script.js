f3ef4723250a8ca005454201a17a05e2f0de31c4
Eltonh <elhay22@gmail.com>
2019-10-03 23:52:36 -0300

    Added konami code easter egg #4

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+// Konami code easter egg
+if ( window.addEventListener ) {
+    var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+    window.addEventListener("keydown", function(e){
+        kkeys.push( e.keyCode );
+        
+        if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+            var audio = document.getElementById("mgsalert");
+            audio.play();
+            alert('kept you waitin, huh?');
+            kkeys = [];
+        }
+    }, true);
+}
