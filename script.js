e6a8efbecc8e15a9d4b3192c13c62460b9eedea2
Deefoozy <tinotrok@outlook.com>
2019-10-04 12:12:27 +0200

    Added console hunt

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// There should be an input somewhere, I don't know where though
+console.log("Let the hunt begin");
 
+let IAM;
+document.querySelector('.input.hidden').addEventListener('change', function() {
+	if (this.value.toLowerCase() === 'found it') {
+		IAM = 'Breathtaking!';
+		alert('Ahh, you found it! You win!');
+		console.log('Try running: "IAM" in your js console!')
+	}
+});
