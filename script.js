fa3647b918ecc2bd252e27d467646bd67e5a2ea3
Michael <mliang.dev@gmail.com>
2019-10-06 11:38:18 -0300

    Add rain code easter egg.

--- script.js
+++ script.js
@@ -65,3 +65,57 @@ document.querySelector('.input.hidden').addEventListener('change', function() {
 		console.log('Try running: "IAM" in your js console!')
 	}
 });
+
+
+// Click specified times to rain words like Matrix movie.
+var counter = 0;
+var times = 10;
+document.querySelector('#codeEffect').addEventListener('click', function() {
+    if (counter === times) {
+        alert('Wecolme to code World!')
+        document.querySelectorAll('body span, p, a').forEach(function(el) {
+            el.style.color = '#FFFFFF';
+        });
+        makeRainCode();
+    }
+    counter++;
+})
+
+function makeRainCode() {
+    var canvasRain = document.getElementById("canvasRain");
+    var ctx = canvasRain.getContext("2d");
+    
+    canvasRain.height = window.innerHeight;
+    canvasRain.width = window.innerWidth;
+    
+    var stringFall = "Calum Dingwall, a Tech Guy";
+    stringFall = stringFall.split("");
+    
+    var font_size = 10;
+    var columns = canvasRain.width / font_size;
+    var drops = [];
+    
+    for (var x = 0; x < columns; x++)
+        drops[x] = 1;
+    
+    function draw() {
+        ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+        ctx.fillRect(0, 0, canvasRain.width, canvasRain.height);
+    
+        ctx.fillStyle = "#0F0"; // This color of chars.
+        ctx.font = font_size + "px Helvetica";
+        for (var i = 0; i < drops.length; i++) {
+            
+            var text = stringFall[Math.floor(Math.random() * stringFall.length)];
+            
+            ctx.fillText(text, i * font_size, drops[i] * font_size);
+    
+            if (drops[i] * font_size > canvasRain.height && Math.random() > 0.975)
+                drops[i] = 0;
+    
+            drops[i]++;
+        }
+    }
+    
+    setInterval(draw, 33);
+}
